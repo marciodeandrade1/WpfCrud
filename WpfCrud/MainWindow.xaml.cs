cf430951d80b6941dfc51b41d0cd7a96e9396be8
Marcio de Andrade <marcio@requestdev.net>
2024-05-02 17:15:53 -0400

    Criação dos métodos CRUD no MainWindow.xaml.cs

--- WpfCrud/MainWindow.xaml.cs
+++ WpfCrud/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfCrud.Data;
 
 namespace WpfCrud
 {
@@ -20,9 +21,47 @@ namespace WpfCrud
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        ProdutoDbContext context;
+        Produto NewProduct = new Produto();
+        Produto selectedProduct = new Produto();
+        public MainWindow(ProdutoDbContext context)
         {
+            this.context = context;
             InitializeComponent();
+            GetProdutos();
+            NewProductGrid.DataContext = context;
+        }
+
+        private void GetProdutos()
+        {
+            ProductDG.ItemsSource = context.Produtos.ToList();
+        }
+
+        private void AddItem(object s, RoutedEventArgs e)
+        {
+            context.Produtos.Add(NewProduct);
+            context.SaveChanges();
+            GetProdutos();
+            NewProduct = new Produto();
+            NewProductGrid.DataContext = NewProduct;
+        }
+        private void UpdateItem(object s, RoutedEventArgs e)
+        {
+            context.Update(selectedProduct);
+            context.SaveChanges();
+            GetProdutos();
+        }
+        private void SelectProductToEdit(object s, RoutedEventArgs e)
+        {
+            selectedProduct = (s as FrameworkElement).DataContext as Produto;
+            UpdateProductGrid.DataContext = selectedProduct;
+        }
+        private void DeleteProduct(object s, RoutedEventArgs e)
+        {
+            var productToDelete = (s as FrameworkElement).DataContext as Produto;
+            context.Produtos.Remove(productToDelete);
+            context.SaveChanges();
+            GetProdutos();
         }
     }
 }
